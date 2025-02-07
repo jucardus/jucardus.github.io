f3dc56ece6d9074e7f52551cc36df4c75cf2f599
Julio Cárdenas Pelizzari <jucardus@tutanota.com>
2025-02-07 09:21:32 -0500

    Update base.js

--- base.js
+++ base.js
@@ -2,7 +2,7 @@ let base = [
   "Busca en Jucardus ¶ /busca ¶ busca, búsqueda, buscas, búsquedas",
   "Inicio ¶ / ¶ inicio, página de inicio, index, página principal",
   "Herramientas web | Generador de claves ¶ /h/claves ¶ generador de claves, generador de contraseñas, claves seguras, contraseñas seguras, número, números, letra, letras, letra mayúscula, letras mayúsculas, caracteres especiales, carácter, carácter especial",
-  "Herramientas web | Enlaces varios ¶ /h/enlaces ¶ enlaces varios, enlace, hipervínculos, hipervínculos varios, vínculos, vínculos varios, enlace, hipervínculo, vinculo, temas, subtemas",
+  "Herramientas web | Enlaces varios ¶ /h/enlaces ¶ enlaces varios, enlace, hipervínculos, hipervínculos varios, vínculos, vínculos varios, enlace, hipervínculo, vinculo, temas, subtemas, sitio, sitios, sitios web, sitio web, página, página web, páginas web, páginas",
   "Herramientas web | Colores CSS con nombre ¶ /h/colores ¶ colores, colores css, color css, color, colores css con nombre, hexadecimal, valores hexadecimales, valor hexadecimal, valor rgb, valores rgb",
   "Escritos varios | Reflexiones ¶ /e/reflexiones ¶ reflexión, reflexiones",
   "Escritos varios | Apotegmas ¶ /e/apotegmas ¶ apotegma, apotegmas, aforismo, aforismos, refrán, refranes, proverbio, proverbios, sentencia, sentencias, dicho, dichos, máxima, máximas",
