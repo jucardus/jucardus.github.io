1f6493ccb6934ab8f65f33bbfe73623a626f3f26
Julio Cárdenas Pelizzari <jucardus@tutanota.com>
2025-02-11 13:58:30 -0500

    Update index.js

--- index.js
+++ index.js
@@ -223,5 +223,5 @@ function buscar (recibidoDireccion) {
 function copiarEnlace (fechaSimpleRecibida) {
   var urlFecha = 'jucardus.github.io/' + fechaSimpleRecibida;
   navigator.clipboard.writeText(urlFecha);
-  buscar(fechaSimpleRecibida);
+  //buscar(fechaSimpleRecibida);
 }
