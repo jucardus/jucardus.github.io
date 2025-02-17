bea8ff7c7e71e64b72356e413f0b4df21193a38f
Jucardus.com <197543233+jucardus@users.noreply.github.com>
2025-02-17 13:53:36 -0500

    Update busca-base.js

--- busca/busca-base.js
+++ busca/busca-base.js
@@ -1,4 +1,10 @@
 let base = [
+  "Apotegmas ¶ Aforismos de mi autoría, parte de <i>Escritos varios</i>. ¶ https://jucardus.github.io/escritos/apotegmas/apotegmas.html",
+  "Citas ¶ Frases célebres, aforismos, refranes, proverbios. Parte de <i>Escritos varios</i>. ¶ https://jucardus.github.io/escritos/citas/citas.html",
+  "Vocabulario español ¶ Algunas palabras españolas seguidas de sus significados. ¶ https://jucardus.github.io/diccionarios/vocabulario/vocabulario.html",
+  "in deference to [loc.] ¶ En el <i>Diccionario inglés-español</i>. En consideración a, por deferencia a, por respeto a. ¶ https://jucardus.github.io/diccionarios/ingles-espanol/i/in-deference-to/in-deference-to.html",
+  "Diccionario inglés-español - I ¶ Entradas iniciadas en I en el diccionario inglés-español. ¶ https://jucardus.github.io/diccionarios/ingles-espanol/i/i.html",
+  "Diccionario inglés-español ¶ Parte de <i>Diccionarios todos</i>. ¶ https://jucardus.github.io/diccionarios/ingles-espanol/ingles-espanol.html",
   "Traducciones al español ¶ Textos traducidos del inglés y del francés al español. ¶ https://jucardus.github.io/traducciones/traducciones.html",
   "Programación básica ¶ Bash, CSS, Blogger, JavaScript, Google Apps Script. ¶ https://jucardus.github.io/programacion/programacion.html",
   "Índice alfabético ¶ Todas las entradas y subpáginas de este sitio, en orden alfabético. ¶ https://jucardus.github.io/indices/alfabetico.html",
