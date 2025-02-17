2a4df7662513cf09ba1b5cd22ce1ad80f86fd4f7
Jucardus.com <197543233+jucardus@users.noreply.github.com>
2025-02-17 13:08:49 -0500

    Create busca-base.js

---
+++ busca/busca-base.js
@@ -0,0 +1,11 @@
+let base = [
+  "Traducciones al español ¶ Textos traducidos del inglés y del francés al español. ¶ https://jucardus.github.io/traducciones/traducciones.html",
+  "Programación básica ¶ Bash, CSS, Blogger, JavaScript, Google Apps Script. ¶ https://jucardus.github.io/programacion/programacion.html",
+  "Índice alfabético ¶ Todas las entradas y subpáginas de este sitio, en orden alfabético. ¶ https://jucardus.github.io/indices/alfabetico.html",
+  "Escritos varios ¶ Apotegmas, cuentos, citas, poesía, reflexiones. ¶ https://jucardus.github.io/escritos/escritos.html",
+  "Enlaces muchos ¶ Enlaces a sitios externos de interés ¶ https://jucardus.github.io/enlaces/enlaces.html",
+  "Diccionarios todos ¶ Inglés-español, chino-español, nombres bíblicos, multilingüe ¶ https://jucardus.github.io/diccionarios/diccionarios.html",
+  "Actividad reciente ¶ Las subpáginas y entradas más recientes de este sitio. ¶ https://jucardus.github.io/actividad/actividad.html",
+  "Repositorio ¶ El repositorio de este sitio, con busca, descarga, copia y otras opciones de GitHub. ¶ https://jucardus.github.io/repo",
+  "Inicio ¶ Página de inicio, con el contenido principal. ¶ https://jucardus.github.io/"
+  ]
