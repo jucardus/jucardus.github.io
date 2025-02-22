125e9172bf458ed5b0f5ca9b02561c546dfef5c8
Jucardus.com <197543233+jucardus@users.noreply.github.com>
2025-02-21 20:03:22 -0500

    Update busca-base.js

--- busca/busca-base.js
+++ busca/busca-base.js
@@ -13,15 +13,15 @@ let base = [
   "Citas ¶ Frases célebres, aforismos, refranes, proverbios. Parte de <i>Escritos varios</i>. ¶ https://jucardus.github.io/escritos/citas/citas.html",
   "Vocabulario español ¶ Algunas palabras españolas seguidas de sus significados. ¶ https://jucardus.github.io/diccionarios/vocabulario/vocabulario.html",
   "in deference to [loc.] ¶ En el <i>Diccionario inglés-español</i>. En consideración a, por deferencia a, por respeto a. ¶ https://jucardus.github.io/diccionarios/ingles-espanol/i/in-deference-to/in-deference-to.html",
-  "Diccionario inglés-español - I ¶ Entradas iniciadas en I en el diccionario inglés-español. ¶ https://jucardus.github.io/diccionarios/ingles-espanol/i/i.html",
+  "Diccionario inglés-español - I ¶ Entradas iniciadas en I en el _Diccionario inglés-español_. ¶ https://jucardus.github.io/diccionarios/ingles-espanol/i/i.html",
   "Diccionario inglés-español ¶ Parte de <i>Diccionarios todos</i>. ¶ https://jucardus.github.io/diccionarios/ingles-espanol/ingles-espanol.html",
   "Traducciones al español ¶ Textos traducidos del inglés y del francés al español. ¶ https://jucardus.github.io/traducciones/traducciones.html",
   "Programación básica ¶ Bash, CSS, Blogger, JavaScript, Google Apps Script. ¶ https://jucardus.github.io/programacion/programacion.html",
   "Índice alfabético ¶ Todas las entradas y subpáginas de este sitio, en orden alfabético. ¶ https://jucardus.github.io/indices/alfabetico.html",
   "Escritos varios ¶ Apotegmas, cuentos, citas, poesía, reflexiones. ¶ https://jucardus.github.io/escritos/escritos.html",
   "Enlaces muchos ¶ Enlaces a sitios externos de interés ¶ https://jucardus.github.io/enlaces/enlaces.html",
-  "Diccionarios todos ¶ Inglés-español, chino-español, nombres bíblicos, multilingüe ¶ https://jucardus.github.io/diccionarios/diccionarios.html",
+  "Diccionarios todos ¶ Inglés-español, chino-español, nombres bíblicos, multilingüe. ¶ https://jucardus.github.io/diccionarios/diccionarios.html",
   "Actividad reciente ¶ Las subpáginas y entradas más recientes de este sitio. ¶ https://jucardus.github.io/actividad/actividad.html",
-  "Repositorio ¶ El repositorio de este sitio, con busca, descarga, copia y otras opciones de GitHub. ¶ https://jucardus.github.io/repo",
+  "Repositorio ¶ El repositorio de este sitio, con busca, descarga, copia y otras opciones de GitHub. ¶ https://github.com/jucardus/jucardus.github.io",
   "Inicio ¶ Página de inicio, con el contenido principal. ¶ https://jucardus.github.io/"
   ]
