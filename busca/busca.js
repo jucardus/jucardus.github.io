03f910b0b7a0cc21df417bc2a0bf03c7f4db65a4
Jucardus.com <197543233+jucardus@users.noreply.github.com>
2025-02-22 12:24:59 -0500

    Update busca.js

--- busca/busca.js
+++ busca/busca.js
@@ -1,21 +1,20 @@
-function recientes() {
-  var array = base;
-  var contador = 0;
-  var resultado = [];
-  let temasTodos = [];
-  for (var i = 0; i < array.length; i++) {
-    var linea = array[i].split(' ¶ ');
-      var tema = linea[0];
-      var subtema = linea[1];
-      var url = linea[2];
-    var nuevaLinea = '<p><b><a href="' + url + '">' + tema + '</a></b> <span class="barrita">&ndash;</span> ' + subtema + '</p>';
-    resultado.push(nuevaLinea);
-    contador = contador + 1;
-    if (contador == 12) {break;}
-  }
-  var enlaces = resultado.join('');
-  document.getElementById('mostrar').innerHTML = enlaces;
-}
+let base = [
+  //'https://github.com/jucardus/jucardus.github.io/blob/main/indices/alfabetico.md',
+  'https://github.com/jucardus/jucardus.github.io/blob/main/traducciones/traducciones.md',
+  'https://github.com/jucardus/jucardus.github.io/blob/main/traducciones/charlie/charlie.md',
+  'https://github.com/jucardus/jucardus.github.io/blob/main/traducciones/charlie/prefacio/prefacio.md',
+  'https://github.com/jucardus/jucardus.github.io/blob/main/actividad/actividad.md',
+  'https://github.com/jucardus/jucardus.github.io/blob/main/diccionarios/diccionarios.md',
+  'https://github.com/jucardus/jucardus.github.io/blob/main/diccionarios/ingles-espanol/ingles-espanol.md',
+  'https://github.com/jucardus/jucardus.github.io/blob/main/diccionarios/ingles-espanol/i/i.md',
+  'https://github.com/jucardus/jucardus.github.io/blob/main/diccionarios/vocabulario/vocabulario.md',
+  'https://github.com/jucardus/jucardus.github.io/blob/main/enlaces/enlaces.md',
+  'https://github.com/jucardus/jucardus.github.io/blob/main/escritos/apotegmas/apotegmas.md',
+  'https://github.com/jucardus/jucardus.github.io/blob/main/escritos/escritos.md',
+  'https://github.com/jucardus/jucardus.github.io/blob/main/escritos/citas/citas.md',
+  'https://github.com/jucardus/jucardus.github.io/blob/main/programacion/programacion.md'
+  ];
+
 function teclaBuscar (event) {
   if (event.key === "Enter") {
     event.preventDefault();
@@ -23,30 +22,33 @@ function teclaBuscar (event) {
   }
 }
 function buscar() {
-  let recibido = document.getElementById("buscar").value;
-  if (recibido == 'mostrar todo') {recibido = ' ¶ ';}
+  let lema = document.getElementById("buscar").value;
+  if (lema == 'mostrar todo') {recibido = '#';}
   var array = base;
   var resultado = [];
-  if (recibido.length >= 2 && recibido != ' ' && recibido != '  ' && recibido != 'temastemastemas') {
-    document.getElementById('mostrar').innerHTML = '<span class="previo">[ BUSCANDO ]</span>';
-    for (var i = 0; i < array.length; i++) {
-      if (array[i].toUpperCase().replace(/ ¶ .*/g,' ¶ ').indexOf(recibido.toUpperCase()) >= 0) {
-        var linea = array[i].split(' ¶ ');
-          var tema = linea[0];
-          var subtema = linea[1];
-          var url = linea[2];
-          var orden = tema.toUpperCase().replace(/ /g,'').replace(/,/g,'').replace(/-/g,'').replace(/;/g,'').replace(/\[/g,'').replace(/\{/g,'').replace(/Á/g,'A').replace(/É/g,'E').replace(/Í/g,'I').replace(/Ó/g,'O').replace(/Ú/g,'U').replace(/Ü/g,'U').replace(/Ñ/g,'N');
-        var nuevaLinea = '<p><!--' + orden + '--><b><a href="' + url + '">' + tema + '</a></b> <span class="barrita">&ndash;</span> ' + subtema + '</p>';
-        resultado.push(nuevaLinea);
-      }
-    }
-    resultado = resultado.sort((a, b) => a.localeCompare(b));
-    var enlaces = resultado.join('<p>');
-    if (enlaces.indexOf('http') == -1) {var enlaces = '<span class="previo">[ ninguna coincidencia ]</span>';}
-    document.getElementById('buscar').value = '';
-    document.getElementById('mostrar').innerHTML = enlaces;
-  } else {
-    document.getElementById('buscar').value = '';
-    document.getElementById('mostrar').innerHTML =  '<span class="previo">[ mínimo dos caracteres de busca ]</span>';
+  for (let i = 0; i < array.length; i++) {
+    recorrer(array[i]);
   }
+}
+
+function recorrer (documento) {
+  fetch(documento)
+    .then(response => response.text())
+    .then(contenidoDoc => {
+      if(buscar(contenidoDoc, lema)) {
+        modificar(documento, contenidoDoc);
+      }    
+    });
+}
+
+function buscar(contenidoDoc,lema) {
+  if (contenidoDoc.indexOf(lema) >= 0) return true;
+  return false;
+}
+
+function modificar (documento, contenidoDoc) {
+  var titulo = contenidoDoc.replace(/## /g,'').replace(/\n.*/g,'');
+  let github = '../';
+  let enlace = '<b>' + titulo + '</b> → <a href="' + github + documento + '" target="_blank">' + documento + "</a><p>";
+  document.getElementById('mostrar').innerHTML +=  enlace;
 }
