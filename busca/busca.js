498703f8cca964016f1cc46422545eaf2fe3bf0e
Jucardus.com <197543233+jucardus@users.noreply.github.com>
2025-02-22 19:23:55 -0500

    Update busca.js

--- busca/busca.js
+++ busca/busca.js
@@ -37,12 +37,15 @@ function buscar2 (contenidoDoc,lema) {
 
 function modificar (documento, contenidoDoc) {
   var titulo = contenidoDoc.replace(/## /g,'').replace(/\n.*/g,'');
-  let enlace = '<b>' + titulo + '</b> → <a class="enlaces" href="' + documento.replace('.md','.html') + '">' + documento.replace('.md','') + "</a><br />";
+  let enlace = '<b>' + titulo + '</b> → <a class="enlaces" href="' + documento.replace('.md','.html') + '">' + documento.replace('.md','').replace(/\//g,' / ') + "</a><br />";
   contador = contador + 1;
   if (enlace.indexOf('../') == -1) {enlace = '<span style="color: OrangeRed;">[ ninguna coincidencia ]</span>';}
   document.getElementById('buscar').value = '';
   document.getElementById('mostrar').innerHTML += enlace;
   document.getElementById('contador').innerHTML = contador + ' concidencias.';
+  if (contador == 0) {
+    document.getElementById('contador').innerHTML = 'Ninguna coincidencia.';
+  }
   window.scrollTo(0, 0);
   document.getElementById('buscar').focus();
 }
