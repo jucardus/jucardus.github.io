156c7ed71dcc9677ab69ec65cb5fdc0651444726
Jucardus.com <197543233+jucardus@users.noreply.github.com>
2025-02-22 19:39:09 -0500

    Update busca.js

--- busca/busca.js
+++ busca/busca.js
@@ -41,7 +41,6 @@ function modificar (documento, contenidoDoc) {
   var titulo = contenidoDoc.replace(/## /g,'').replace(/\n.*/g,'');
   let enlace = '<b>' + titulo + '</b> → <a class="enlaces" href="' + documento.replace('.md','.html') + '">' + documento.replace('.md','').replace(/\//g,' / ') + "</a><br />";
   //contador = contador + 1;
-  alert (contador);
   if (contador == 0) {
     document.getElementById('contador').innerHTML = 'Ninguna coincidencia.';
   }
