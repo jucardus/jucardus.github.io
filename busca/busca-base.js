c2f9b07c28a7852b54726338dbded7bf929fe2d4
Jucardus.com <197543233+jucardus@users.noreply.github.com>
2025-03-01 11:55:19 -0500

    Update busca-base.js

--- busca/busca-base.js
+++ busca/busca-base.js
@@ -14,7 +14,6 @@ let base = [
   'https://github.com/jucardus/jucardus.github.io/blob/main/musica/musica.md',
   'https://github.com/jucardus/jucardus.github.io/blob/main/diccionarios/ingles-espanol/i/in-deference-to/in-deference-to.md',
   'https://github.com/jucardus/jucardus.github.io/blob/main/index.md',
-  'https://github.com/jucardus/jucardus.github.io/blob/main/indices/alfabetico.html',
   'https://github.com/jucardus/jucardus.github.io/blob/main/traducciones/traducciones.md',
   'https://github.com/jucardus/jucardus.github.io/blob/main/traducciones/charlie/charlie.md',
   'https://github.com/jucardus/jucardus.github.io/blob/main/traducciones/charlie/prefacio/prefacio.md',
