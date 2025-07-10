92bff1c2dc8677dd8b091445b5997029e2622f4c
Jucardus.com <197543233+jucardus@users.noreply.github.com>
2025-07-10 11:08:33 -0500

    Update index.js

--- index.js
+++ index.js
@@ -213,7 +213,7 @@ function archivo() {
         var etiquetas = linea[3];
         var fecha = linea[4];
         var contenido = linea[5];
-      var nuevaLinea = '<p><span class="subtituloBuscar" onclick="buscarFecha(\'' + fecha + '\')">' + numero + '. ' + titulo + '</span> – <span class="etiquetasArchivo">' + etiquetas + '</span></p>';
+      var nuevaLinea = '<p><span class="subtituloBuscar" onclick="buscarFecha(\'' + fecha + '\')"><b>' + numero + '.</b> ' + titulo + '</span> – <span class="etiquetasArchivo">' + etiquetas + '</span></p>';
       resultado.push('<h2 style="text-align: center; padding-top: 15px;">' + fecha.slice(0, 7) + '</h2></div>');
       resultado.push('<h4>' + fecha.slice(0, 10) + '</h4>');
       resultado.push(nuevaLinea);
