efe2df59f139694b769d7967514810097e9dd98a
Jucardus.com <197543233+jucardus@users.noreply.github.com>
2025-07-16 18:58:59 -0500

    Update index.js

--- index.js
+++ index.js
@@ -107,8 +107,8 @@ function buscarFecha (recibido) {
         var titulo = linea[1];
         var numero = linea[2];
           var enlaceTuit = 'jucardus.github.io/?e=' + recibido.replace(/-/g,'').replace(/:/g,'').replace(/ /g,'').slice(2);
-          var tuit = numero + '. ' + titulo + ' – ' + linea[5]?.slice(0,180) + '...%0A→ ' + enlaceTuit + '%0A%0A' + '@jucardus';
-          var numero = '<a class="numeros" href="https://x.com/intent/tweet?text=' + tuit + '">' + numero + '</a>';
+          var tuit = numero + '. ' + titulo + '%0A%0A' + linea[5]?.slice(0,180).replace(/<[^>]*>?/gm, '') + '...%0A%0A→ ' + enlaceTuit + '%0A%0A' + '@jucardus';
+          var numero = '<a class="numeros" target="_blank" href="https://x.com/intent/tweet?text=' + tuit + '">' + numero + '</a>';
         var etiquetas = linea[3];
           var etiquetas = etiquetas.replace(/, /g, ',');
           var arrayEtiquetas = etiquetas.split(',');
