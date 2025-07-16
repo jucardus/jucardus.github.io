e49eba81e7221427c94287d6fb901dc69fcd83f6
Jucardus.com <197543233+jucardus@users.noreply.github.com>
2025-07-16 10:55:06 -0500

    Update index.js

--- index.js
+++ index.js
@@ -333,15 +333,15 @@ document.addEventListener('DOMContentLoaded', () => {
   if (recibido) {
     let originalString = recibido;
     let characterToInsert = "-";
-    let insertionPosition = 2;
+    var insertionPosition = 2;
     let newString = originalString.slice(0, insertionPosition) + characterToInsert + originalString.slice(insertionPosition);
-    let insertionPosition = 5;
+    var insertionPosition = 5;
     let newString = originalString.slice(0, insertionPosition) + characterToInsert + originalString.slice(insertionPosition);
     let characterToInsert = " ";
-    let insertionPosition = 8;
+    var insertionPosition = 8;
     let newString = originalString.slice(0, insertionPosition) + characterToInsert + originalString.slice(insertionPosition);
     let characterToInsert = ":";
-    let insertionPosition = 11;
+    var insertionPosition = 11;
     let newString = originalString.slice(0, insertionPosition) + characterToInsert + originalString.slice(insertionPosition);
     var recibido = '20' + newString;
     buscarFecha(recibido);
