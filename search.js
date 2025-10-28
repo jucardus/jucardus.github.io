66dc9539c084ad3019a60a745d7c2c2576aaeb3c
Jucardus <197543233+jucardus@users.noreply.github.com>
2025-10-28 08:45:15 -0500

    Create search.js

---
+++ search.js
@@ -0,0 +1,5 @@
+base = [
+"<a href='https://github.com/jucardus/jucardus.github.io/blob/main/a/b/a/abacus-s.md"><b>abacus [s.]</b> - Diccionario inglés-español, Sustantivos ingleses - 25-10-26</a>",
+"<a href='https://github.com/jucardus/jucardus.github.io/blob/main/a/c/t/actualizacion-de-linux-mint-con-bash.md'><b>Actualización de Linux Mint con Bash</b> - Bash, Programación, Linux Mint - 25-10-27</a>",
+"<a href='https://github.com/jucardus/jucardus.github.io/blob/main/t/h/e/the-priestly-confession-in-the-catholic-church.md'><b>The priestly confession in the Catholic Church</b> - Catolicismo, Según Grok - 25-10-26</a>"
+]
