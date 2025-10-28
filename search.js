83ad565dbbaaecabadb23d9b406e6a45bc609591
Jucardus <197543233+jucardus@users.noreply.github.com>
2025-10-28 10:20:11 -0500

    Add files via upload

--- search.js
+++ search.js
@@ -1,5 +1,5 @@
 base = [
-"<a href='https://jucardus.github.io/a/b/a/abacus-s'><b>abacus [s.]</b> - Diccionario inglés-español, Sustantivos ingleses - 25-10-26</a>",
-"<a href='https://jucardus.github.io/a/c/t/actualizacion-de-linux-mint-con-bash'><b>Actualización de Linux Mint con Bash</b> - Bash, Programación, Linux Mint - 25-10-27</a>",
-"<a href='https://jucardus.github.io/t/h/e/the-priestly-confession-in-the-catholic-church'><b>The priestly confession in the Catholic Church</b> - Catolicismo, Según Grok - 25-10-26</a>"
+"<a href='jucardus.github.io/github-1#/a/b/a/abacus-s'><b>abacus [s.]</b> - Diccionario inglés-español, Sustantivos ingleses - 25-10-26</a>",
+"<a href='jucardus.github.io/github-1#/a/c/t/actualizacion-de-linux-mint-con-bash'><b>Actualización de Linux Mint con Bash</b> - Bash, Programación, Linux Mint - 25-10-27</a>",
+"<a href='jucardus.github.io/github-1#/t/h/e/the-priestly-confession-in-the-catholic-church'><b>The priestly confession in the Catholic Church</b> - Catolicismo, Según Grok - 25-10-26</a>"
 ]
