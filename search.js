de576f944795c421f33e5910b1d30c0fdd09249d
Jucardus <197543233+jucardus@users.noreply.github.com>
2025-11-04 12:47:21 -0500

    Update search.js

--- search.js
+++ search.js
@@ -1,5 +1,4 @@
 let entriesBase = [
-  "▓[Actividad reciente](https://jucardus.github.io/actividad) en [Jucardus](https://jucardus.github.io/)▒readme.md▓",
   "▓abacus [s.]▒a/b/a/abacus-s.md▓",
   "▓abacus [s.] · Pronunciación▒a/b/a/abacus-s.md#pronunciaci%C3%B3n▓",
   "▓abacus [s.] · Traducciones▒a/b/a/abacus-s.md#traducciones▓",
