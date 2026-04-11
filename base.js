1927c3711ca240109dce7eaf7a0dc4f2b61e4dc3
Jucardus <197543233+jucardus@users.noreply.github.com>
2026-04-11 17:11:23 -0500

    Update base.js from Google Drive

--- base.js
+++ base.js
@@ -1,4 +1,5 @@
 let base = [
+  "▓brazaletes inteligentes▒NTAG215 NFC▒https://www.temu.com/pe-en/nfc-cards-nfc-tags-nfc-business-cards-nfc-215-cards-blank-nfc-cards-with-504--programmable-nfc-tags--card-wristbands-tap-to-share-your-nfc-business-card--wristband-g-601100674688801.html?refer_page_name=goods&refer_page_id=10032_1771184830179_medoi84alc&refer_page_sn=10032&_x_sessn_id=4d1omysu1v&is_back=1&no_cache_id=gc3op▓",
   "▓anillos inteligentes▒Anillo Inteligente R5 Jakcom▒https://www.mercadolibre.com.pe/anillo-inteligente-nfc-resistente-al-agua-para-moviles/up/MPEU3479660340#polycard_client=search-desktop&search_layout=grid&position=2&type=product&tracking_id=4b388825-b4cc-46fa-8d8b-27cd439b554e&wid=MPE840833008&sid=search▓",
   "▓mis sitios▒Repositorio con todo▒https://jucardus.github.io/repositorio▓",
   "▓vocabulario español▒pichulear▒https://www.diccionarioargentino.com/term/Pichulear▓",
