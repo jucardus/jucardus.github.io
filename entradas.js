ff26fe0f61540de0ffb8ad4967c4b03e3418509a
Jucardus <197543233+jucardus@users.noreply.github.com>
2026-05-17 21:22:22 -0500

    Create entradas.js

---
+++ entradas.js
@@ -0,0 +1,4 @@
+let entradas = [
+  "▓aaa – aaa, bbb▒▒https://jucardus.github.io/a/a/a/aaa.html▓",
+  "▓abacus – Vocabulario inglés, Sustantivos ingleses▒▒https://jucardus.github.io/a/b/a/abacus.html▓"
+];
