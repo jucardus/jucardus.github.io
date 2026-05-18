fe01cab59ddbc85f0b32d15aeb0e6db3f5c44c3c
Jucardus <197543233+jucardus@users.noreply.github.com>
2026-05-18 11:14:49 -0500

    Update entradas.js with "cabeza de familia [m.f.c.]"

--- entradas.js
+++ entradas.js
@@ -1,5 +1,6 @@
 var entradas = [
   "▓aaa – aaa▒https://jucardus.github.io/a/a/a/aaa.html▓",
   "▓abacus – Vocabulario inglés, Sustantivos ingleses▒https://jucardus.github.io/a/b/a/abacus.html▓",
-  "▓bbb – bbb▒https://jucardus.github.io/b/b/b/bbb.html▓"
+  "▓bbb – bbb▒https://jucardus.github.io/b/b/b/bbb.html▓",
+  "▓cabeza de familia [m.f.c.] – Diccionario multilingüe▒https://jucardus.github.io/c/a/b/cabeza-de-familia-mfc.html▓"
 ];
