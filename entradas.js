50defb61024780a4a1586ea4836e8877f334aef0
Jucardus <197543233+jucardus@users.noreply.github.com>
2026-05-18 12:07:32 -0500

    Update entradas.js with "bbb"

--- entradas.js
+++ entradas.js
@@ -4,6 +4,6 @@ var entradas = [
   "▓abacus – Vocabulario inglés, Sustantivos ingleses▒https://jucardus.github.io/a/b/a/abacus.html▓",
   "▓abdomen [m.] – Diccionario multilingüe - A▒https://jucardus.github.io/a/b/d/abdomen-m.html▓",
   "▓afectuoso [adj.] – Diccionario multilingüe - A▒https://jucardus.github.io/a/f/e/afectuoso-adj.html▓",
-  "▓bbb – bbb▒https://jucardus.github.io/b/b/b/bbb.html▓",
+  "▓bbb – aaa▒https://jucardus.github.io/b/b/b/bbb.html▓",
   "▓cabeza de familia [m.f.c.] – Diccionario multilingüe - C▒https://jucardus.github.io/c/a/b/cabeza-de-familia-mfc.html▓"
 ];
