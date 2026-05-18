94fde282a28a094077ebd713eae4295dee4afb1e
Jucardus <197543233+jucardus@users.noreply.github.com>
2026-05-18 12:23:21 -0500

    Update entradas.js with "aaa ccc ddd"

--- entradas.js
+++ entradas.js
@@ -3,6 +3,7 @@ var entradas = [
   "▓aaa aaa abc – aaa▒https://jucardus.github.io/a/a/a/aaa-aaa-abc.html▓",
   "▓aaa bbb – aaa▒https://jucardus.github.io/a/a/a/aaa-bbb.html▓",
   "▓aaa bbb ccc – aaa▒https://jucardus.github.io/a/a/a/aaa-bbb-ccc.html▓",
+  "▓aaa ccc ddd – aaa▒https://jucardus.github.io/a/a/a/aaa-ccc-ddd.html▓",
   "▓abacus – Vocabulario inglés, Sustantivos ingleses▒https://jucardus.github.io/a/b/a/abacus.html▓",
   "▓abdomen [m.] – Diccionario multilingüe - A▒https://jucardus.github.io/a/b/d/abdomen-m.html▓",
   "▓afectuoso [adj.] – Diccionario multilingüe - A▒https://jucardus.github.io/a/f/e/afectuoso-adj.html▓",
