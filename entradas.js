e4222d81ad0fa8e81efa7305880e5192ff438ca5
Jucardus <197543233+jucardus@users.noreply.github.com>
2026-05-18 08:58:04 -0500

    Update entradas.js

--- entradas.js
+++ entradas.js
@@ -1,9 +1,5 @@
-// ==================== ENTRADAS DATABASE ====================
-// This file is auto-generated. Do not edit manually.
-// Format: ▓title – labels▒url▓
-
 const entradas = [
-  ▓aaa – aaa, bbb▒https://jucardus.github.io/a/a/a/aaa.html▓,
-  ▓abacus – Vocabulario inglés, Sustantivos ingleses▒https://jucardus.github.io/a/b/a/abacus.html▓,
-  ▓title – labels▒url▓
+  "▓aaa – aaa, bbb▒https://jucardus.github.io/a/a/a/aaa.html▓",
+  "▓abacus – Vocabulario inglés, Sustantivos ingleses▒https://jucardus.github.io/a/b/a/abacus.html▓",
+  "▓title – labels▒url▓"
 ];
