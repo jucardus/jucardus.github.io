6f78ca6d83b69040067e5f465efb7b79abff954c
Jucardus <197543233+jucardus@users.noreply.github.com>
2026-05-17 22:25:27 -0500

    Update entradas.js with "aaa"

--- entradas.js
+++ entradas.js
@@ -4,5 +4,6 @@
 
 const entradas = [
   ▓aaa – aaa, bbb▒https://jucardus.github.io/a/a/a/aaa.html▓,
-  ▓abacus – Vocabulario inglés, Sustantivos ingleses▒https://jucardus.github.io/a/b/a/abacus.html▓
+  ▓abacus – Vocabulario inglés, Sustantivos ingleses▒https://jucardus.github.io/a/b/a/abacus.html▓,
+  ▓title – labels▒url▓
 ];
