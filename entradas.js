545fbfd04ef7bb29da55de62a9ca0b6c6fb061d8
Jucardus <197543233+jucardus@users.noreply.github.com>
2026-05-18 14:35:52 -0500

    Update entradas.js with "abdicar [v.]"

--- entradas.js
+++ entradas.js
@@ -1,4 +1,5 @@
 var entradas = [
+  "▓abdicar [v.] – Diccionario multilingüe - A▒https://jucardus.github.io/a/b/d/abdicar-v.html▓",
   "▓abdomen [m.] – Diccionario multilingüe - A▒https://jucardus.github.io/a/b/d/abdomen-m.html▓",
   "▓afectuoso [adj.] – Diccionario multilingüe - A▒https://jucardus.github.io/a/f/e/afectuoso-adj.html▓",
   "▓cabeza de familia [m.f.c.] – Diccionario multilingüe - C▒https://jucardus.github.io/c/a/b/cabeza-de-familia-mfc.html▓"
