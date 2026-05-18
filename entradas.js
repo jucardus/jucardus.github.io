033c08e971be6ae0a5a0477f322527222f9b2c81
Jucardus <197543233+jucardus@users.noreply.github.com>
2026-05-18 14:57:14 -0500

    Update entradas.js with "bóveda [f.]"

--- entradas.js
+++ entradas.js
@@ -2,5 +2,6 @@ var entradas = [
   "▓abdicar [v.] – Diccionario multilingüe - A▒https://jucardus.github.io/a/b/d/abdicar-v.html▓",
   "▓abdomen [m.] – Diccionario multilingüe - A▒https://jucardus.github.io/a/b/d/abdomen-m.html▓",
   "▓afectuoso [adj.] – Diccionario multilingüe - A▒https://jucardus.github.io/a/f/e/afectuoso-adj.html▓",
+  "▓bóveda [f.] – Diccionario multilingüe - B▒https://jucardus.github.io/b/o/v/boveda-f.html▓",
   "▓cabeza de familia [m.f.c.] – Diccionario multilingüe - C▒https://jucardus.github.io/c/a/b/cabeza-de-familia-mfc.html▓"
 ];
