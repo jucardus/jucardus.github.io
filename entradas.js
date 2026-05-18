78932bc32584c1d691701794edf4b4356c74916b
Jucardus <197543233+jucardus@users.noreply.github.com>
2026-05-17 21:31:47 -0500

    Update entradas.js

--- entradas.js
+++ entradas.js
@@ -1,4 +1,4 @@
 let entradas = [
-  "▓aaa – aaa, bbb▒https://jucardus.github.io/a/a/a/aaa.html▓",
+  "▓bbb – ccc, ddd▒https://jucardus.github.io/a/a/a/aaa.html▓",
   "▓abacus – Vocabulario inglés, Sustantivos ingleses▒https://jucardus.github.io/a/b/a/abacus.html▓"
 ];
