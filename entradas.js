110fb36464c02183227aaa6a4071d56dd7fbbfcf
Jucardus <197543233+jucardus@users.noreply.github.com>
2026-05-18 13:59:43 -0500

    Update entradas.js

--- entradas.js
+++ entradas.js
@@ -1,17 +1,5 @@
 var entradas = [
-  "▓aaa – aaa▒https://jucardus.github.io/a/a/a/aaa.html▓",
-  "▓aaa aaa abc – aaa▒https://jucardus.github.io/a/a/a/aaa-aaa-abc.html▓",
-  "▓aaa bbb – aaa▒https://jucardus.github.io/a/a/a/aaa-bbb.html▓",
-  "▓aaa bbb ccc – aaa▒https://jucardus.github.io/a/a/a/aaa-bbb-ccc.html▓",
-  "▓aaa ccc ddd – aaa▒https://jucardus.github.io/a/a/a/aaa-ccc-ddd.html▓",
-  "▓aaa ddd ddd – aaa▒https://jucardus.github.io/a/a/a/aaa-ddd-ddd.html▓",
-  "▓aaa eee eee – aaa▒https://jucardus.github.io/a/a/a/aaa-eee-eee.html▓",
-  "▓aaa fff fff – aaa▒https://jucardus.github.io/a/a/a/aaa-fff-fff.html▓",
-  "▓aaa ggg ggg – aaa▒https://jucardus.github.io/a/a/a/aaa-ggg-ggg.html▓",
-  "▓abacus – Vocabulario inglés, Sustantivos ingleses▒https://jucardus.github.io/a/b/a/abacus.html▓",
   "▓abdomen [m.] – Diccionario multilingüe - A▒https://jucardus.github.io/a/b/d/abdomen-m.html▓",
   "▓afectuoso [adj.] – Diccionario multilingüe - A▒https://jucardus.github.io/a/f/e/afectuoso-adj.html▓",
-  "▓bbb – aaa▒https://jucardus.github.io/b/b/b/bbb.html▓",
-  "▓bbb ccc – aaa▒https://jucardus.github.io/b/b/b/bbb-ccc.html▓",
   "▓cabeza de familia [m.f.c.] – Diccionario multilingüe - C▒https://jucardus.github.io/c/a/b/cabeza-de-familia-mfc.html▓"
 ];
