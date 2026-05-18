4a10c0d6efd1d90472922f60975f98f073f97435
Jucardus <197543233+jucardus@users.noreply.github.com>
2026-05-17 21:41:53 -0500

    Update base.js from Google Drive

--- base.js
+++ base.js
@@ -1,4 +1,5 @@
 let base = [
+  "▓celulares pequeños▒AGM M10 | Stylish Powerful Rugged Smartphone 2025 Official Durability Test & Hands-on !!▒https://www.youtube.com/watch?v=tYF2ZDQhd_g▓",
   "▓celulares pequeños▒AGM M10 Rugged Flip Phone #AGMM10 #RuggedFlipPhone▒https://www.youtube.com/watch?v=XbaZrmuZon4▓",
   "▓Diccionario inglés-español▒good riddance [interj.] por fin; al fin; qué alivio▒jucardus▓",
   "▓Diccionario inglés-español▒spite [s.] despecho, rencor, resentimiento▒jucardus▓",
