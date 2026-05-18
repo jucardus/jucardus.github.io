1df5152593d6fa69f13812483a35c893f5c962f1
Jucardus <197543233+jucardus@users.noreply.github.com>
2026-05-18 12:07:46 -0500

    Update entradas.js with "bbb ccc"

--- entradas.js
+++ entradas.js
@@ -5,5 +5,6 @@ var entradas = [
   "▓abdomen [m.] – Diccionario multilingüe - A▒https://jucardus.github.io/a/b/d/abdomen-m.html▓",
   "▓afectuoso [adj.] – Diccionario multilingüe - A▒https://jucardus.github.io/a/f/e/afectuoso-adj.html▓",
   "▓bbb – aaa▒https://jucardus.github.io/b/b/b/bbb.html▓",
+  "▓bbb ccc – aaa▒https://jucardus.github.io/b/b/b/bbb-ccc.html▓",
   "▓cabeza de familia [m.f.c.] – Diccionario multilingüe - C▒https://jucardus.github.io/c/a/b/cabeza-de-familia-mfc.html▓"
 ];
