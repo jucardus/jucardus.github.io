833433e735a409875149c28b68acd034ebdf6788
Jucardus <197543233+jucardus@users.noreply.github.com>
2026-05-18 11:52:05 -0500

    Update entradas.js with "abdomen [m.]"

--- entradas.js
+++ entradas.js
@@ -1,6 +1,7 @@
 var entradas = [
   "▓aaa – aaa▒https://jucardus.github.io/a/a/a/aaa.html▓",
   "▓abacus – Vocabulario inglés, Sustantivos ingleses▒https://jucardus.github.io/a/b/a/abacus.html▓",
+  "▓abdomen [m.] – Diccionario multilingüe - A▒https://jucardus.github.io/a/b/d/abdomen-m.html▓",
   "▓afectuoso [adj.] – Diccionario multilingüe▒https://jucardus.github.io/a/f/e/afectuoso-adj.html▓",
   "▓bbb – bbb▒https://jucardus.github.io/b/b/b/bbb.html▓",
   "▓cabeza de familia [m.f.c.] – Diccionario multilingüe▒https://jucardus.github.io/c/a/b/cabeza-de-familia-mfc.html▓"
