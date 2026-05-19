1682aa2fa5f05cae26eb3b95a781b9339a854d62
Jucardus <197543233+jucardus@users.noreply.github.com>
2026-05-18 20:32:31 -0500

    Update entradas.js with "Admiro a los hombres que buscan el orden..."

--- entradas.js
+++ entradas.js
@@ -3,6 +3,7 @@ var entradas = [
   "▓abacero, -ra [m.f.] – Diccionario multilingüe - A▒https://jucardus.github.io/a/b/a/abacero-ra-mf.html▓",
   "▓abdicar [v.] – Diccionario multilingüe - A▒https://jucardus.github.io/a/b/d/abdicar-v.html▓",
   "▓abdomen [m.] – Diccionario multilingüe - A▒https://jucardus.github.io/a/b/d/abdomen-m.html▓",
+  "▓Admiro a los hombres que buscan el orden... – Citas y proverbios▒https://jucardus.github.io/a/d/m/admiro-a-los-hombres-que-buscan-el-orden.html▓",
   "▓afectuoso [adj.] – Diccionario multilingüe - A▒https://jucardus.github.io/a/f/e/afectuoso-adj.html▓",
   "▓bóveda [f.] – Diccionario multilingüe - B▒https://jucardus.github.io/b/o/v/boveda-f.html▓",
   "▓cabeza de familia [m.f.c.] – Diccionario multilingüe - C▒https://jucardus.github.io/c/a/b/cabeza-de-familia-mfc.html▓",
