38a4289eac63cce124341e4e08c2123073400ad8
Jucardus <197543233+jucardus@users.noreply.github.com>
2026-05-19 14:08:12 -0500

    Update entradas.js with "Diccionario multilingüe"

--- entradas.js
+++ entradas.js
@@ -13,6 +13,7 @@ var entradas = [
   "▓cabeza de familia [m.f.c.] – Diccionario multilingüe - C▒https://jucardus.github.io/c/a/b/cabeza-de-familia-mfc.html▓",
   "▓Cuando exponer un delito es tratado como... – Citas y proverbios▒https://jucardus.github.io/c/u/a/cuando-exponer-un-delito-es-tratado-como.html▓",
   "▓destreza [f.] – Diccionario multilingüe - D, Sustantivos multilingües, Sustantivos multilingües femeninos▒https://jucardus.github.io/d/e/s/destreza-f.html▓",
+  "▓Diccionario multilingüe – Diccionarios▒https://jucardus.github.io/d/i/c/diccionario-multilingue.html▓",
   "▓en un abrir y cerrar de ojos [loc.] – Diccionario multilingüe - E, Locuciones multilingües▒https://jucardus.github.io/e/n/-/en-un-abrir-y-cerrar-de-ojos-loc.html▓",
   "▓fila [f.] – Diccionario multilingüe - F, Sustantivos multilingües, Sustantivos multilingües femeninos▒https://jucardus.github.io/f/i/l/fila-f.html▓",
   "▓Una nación de ovejas... – Citas y proverbios▒https://jucardus.github.io/u/n/a/una-nacion-de-ovejas.html▓"
