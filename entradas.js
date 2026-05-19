c91ba06216ada5a75b370224a33e9dcfe84ff023
Jucardus <197543233+jucardus@users.noreply.github.com>
2026-05-18 20:06:52 -0500

    Update entradas.js with "Una nación de ovejas..."

--- entradas.js
+++ entradas.js
@@ -4,5 +4,6 @@ var entradas = [
   "▓abdomen [m.] – Diccionario multilingüe - A▒https://jucardus.github.io/a/b/d/abdomen-m.html▓",
   "▓afectuoso [adj.] – Diccionario multilingüe - A▒https://jucardus.github.io/a/f/e/afectuoso-adj.html▓",
   "▓bóveda [f.] – Diccionario multilingüe - B▒https://jucardus.github.io/b/o/v/boveda-f.html▓",
-  "▓cabeza de familia [m.f.c.] – Diccionario multilingüe - C▒https://jucardus.github.io/c/a/b/cabeza-de-familia-mfc.html▓"
+  "▓cabeza de familia [m.f.c.] – Diccionario multilingüe - C▒https://jucardus.github.io/c/a/b/cabeza-de-familia-mfc.html▓",
+  "▓Una nación de ovejas... – Citas▒https://jucardus.github.io/u/n/a/una-nacion-de-ovejas.html▓"
 ];
