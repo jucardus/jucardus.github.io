75d1bab1e8d9f26c258b88ee0765de5095a765cb
Jucardus <197543233+jucardus@users.noreply.github.com>
2026-05-23 10:55:06 -0500

    Update entradas.js

--- entradas.js
+++ entradas.js
@@ -26,7 +26,6 @@ var entradas = [
   "▓logis [m.] – Sustantivos franceses, Sustantivos franceses masculinos, Vocabulario francés - L▒https://jucardus.github.io/l/o/g/logis-m▓",
   "▓Los viejos tiempos nunca vuelven, porque... – Apotegmas▒https://jucardus.github.io/l/o/s/los-viejos-tiempos-nunca-vuelven-porque▓",
   "▓noblesse de cloche [f.c.] – Sustantivos franceses compuestos, Sustantivos franceses femeninos, Vocabulario francés - N▒https://jucardus.github.io/n/o/b/noblesse-de-cloche-fc▓",
-  "▓œillet [m.] – Sustantivos franceses, Sustantivos franceses masculinos, Vocabulario francés - O▒https://jucardus.github.io/i/l/l/illet-m▓",
   "▓œillet [m.] – Sustantivos franceses, Sustantivos franceses masculinos, Vocabulario francés - O▒https://jucardus.github.io/o/e/i/oeillet-m▓",
   "▓quoique [conj.] – Conjunciones francesas, Vocabulario francés - Q▒https://jucardus.github.io/q/u/o/quoique-conj▓",
   "▓rez-de-chaussée [m.c.] – Sustantivos franceses, Sustantivos franceses compuestos, Sustantivos franceses masculinos, Vocabulario francés - R▒https://jucardus.github.io/r/e/z/rez-de-chaussee-mc▓",
