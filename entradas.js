f60448ee1772558cdf744a570805dfe81031904c
Jucardus <197543233+jucardus@users.noreply.github.com>
2026-05-26 10:51:37 -0500

    Update entradas.js

--- entradas.js
+++ entradas.js
@@ -15,7 +15,6 @@ var entradas = [
   "▓Al perro que tiene dinero se... – Citas y proverbios, Citas y proverbios - A▒https://jucardus.github.io/a/l/-/al-perro-que-tiene-dinero-se▓",
   "▓Animales puros e impuros en el cristianismo – Cristianismo, Reflexiones▒https://jucardus.github.io/a/n/i/animales-puros-e-impuros-en-el-cristianismo▓",
   "▓A veces buscamos respuestas rápidas... – Citas y proverbios▒https://jucardus.github.io/a/-/v/a-veces-buscamos-respuestas-rapidas▓",
-  "▓A veces buscamos respuestas rápidas... – Citas y proverbios, Citas y proverbios - A▒https://jucardus.github.io/a/a/v/a-veces-buscamos-respuestas-rapidas▓",
   "▓buis [m.] – Sustantivos franceses, Sustantivos franceses masculinos, Vocabulario francés - B▒https://jucardus.github.io/b/u/i/buis-m▓",
   "▓Buscamos a Dios porque hemos dejado de creer... – Apotegmas▒https://jucardus.github.io/b/u/s/buscamos-a-dios-porque-hemos-dejado-de-creer▓",
   "▓bóveda [f.] – Diccionario multilingüe - B, Sustantivos multilingües, Sustantivos multilingües femeninos▒https://jucardus.github.io/b/o/v/boveda-f▓",
