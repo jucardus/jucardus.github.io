de9fecb6add3dffe6dc8586c16e6e93e8a8fec1a
Jucardus <197543233+jucardus@users.noreply.github.com>
2026-05-26 11:06:41 -0500

    Update entradas.js with "A veces buscamos respuestas rápidas..."

--- entradas.js
+++ entradas.js
@@ -14,7 +14,7 @@ var entradas = [
   "▓a lo largo de todo el año [loc.] – Diccionario multilingüe - A, Locuciones multilingües▒https://jucardus.github.io/a/-/l/a-lo-largo-de-todo-el-ano-loc▓",
   "▓Al perro que tiene dinero se... – Citas y proverbios, Citas y proverbios - A▒https://jucardus.github.io/a/l/-/al-perro-que-tiene-dinero-se▓",
   "▓Animales puros e impuros en el cristianismo – Cristianismo, Reflexiones▒https://jucardus.github.io/a/n/i/animales-puros-e-impuros-en-el-cristianismo▓",
-  "▓A veces buscamos respuestas rápidas... – Citas y proverbios▒https://jucardus.github.io/a/-/v/a-veces-buscamos-respuestas-rapidas▓",
+  "▓A veces buscamos respuestas rápidas... – Citas y proverbios, Citas y proverbios - A▒https://jucardus.github.io/a/-/v/a-veces-buscamos-respuestas-rapidas▓",
   "▓A veces buscamos respuestas rápidas... – Citas y proverbios, Citas y proverbios - A▒https://jucardus.github.io/a/a/v/a-veces-buscamos-respuestas-rapidas▓",
   "▓buis [m.] – Sustantivos franceses, Sustantivos franceses masculinos, Vocabulario francés - B▒https://jucardus.github.io/b/u/i/buis-m▓",
   "▓Buscamos a Dios porque hemos dejado de creer... – Apotegmas▒https://jucardus.github.io/b/u/s/buscamos-a-dios-porque-hemos-dejado-de-creer▓",
