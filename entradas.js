1fb16c4c60acb31ce09f6c3eb0a93e01df268e63
Jucardus <197543233+jucardus@users.noreply.github.com>
2026-05-27 10:38:48 -0500

    Update entradas.js with "Si quieres saber quién te controla..."

--- entradas.js
+++ entradas.js
@@ -53,6 +53,7 @@ var entradas = [
   "▓rez-de-chaussée [m.c.] – Sustantivos franceses, Sustantivos franceses compuestos, Sustantivos franceses masculinos, Vocabulario francés - R▒https://jucardus.github.io/r/e/z/rez-de-chaussee-mc▓",
   "▓scrape [s.] – Sustantivos ingleses, Vocabulario inglés - S▒https://jucardus.github.io/s/c/r/scrape-s▓",
   "▓Siempre hay alguien orando por ti – Reflexiones▒https://jucardus.github.io/s/i/e/siempre-hay-alguien-orando-por-ti▓",
+  "▓Si quieres saber quién te controla... – Citas de Voltaire, Citas y proverbios, Citas y proverbios - S▒https://jucardus.github.io/s/i/-/si-quieres-saber-quien-te-controla▓",
   "▓Son demasiados los presupuestos para poner... – Reflexiones▒https://jucardus.github.io/s/o/n/son-demasiados-los-presupuestos-para-poner▓",
   "▓Sí. Una de las condiciones para que la democracia... – Reflexiones▒https://jucardus.github.io/s/i/-/si-una-de-las-condiciones-para-que-la-democracia▓",
   "▓tallow-chandler [s.c.] – Sustantivos compuestos ingleses, Vocabulario inglés - T▒https://jucardus.github.io/t/a/l/tallow-chandler-sc▓",
