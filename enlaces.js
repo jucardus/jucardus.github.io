a023dfc0a96d97202a2dde22878ec63a71476838
Jucardus <197543233+jucardus@users.noreply.github.com>
2026-06-09 11:27:20 -0500

    Update enlaces.js from Google Apps Script

--- enlaces.js
+++ enlaces.js
@@ -21,7 +21,6 @@ var enlaces = [
   "▓vocabulario español▒brindis al sol▒https://dle.rae.es/brindis#G9S23Eg▒xjn8zp3u▓",
   "▓Diccionario inglés-español *▒<b>kowtow [v.]</b> /ˌkaʊˈtaʊ/ (Del chino 叩頭 (叩头) kòutóu [v.] dar un cabezazo; arrodillarse hasta tocar el suelo con la frente) -- adular servilmente, arrastrarse, arrodillarse servilmente (hasta tocar el suelo con la cabeza), doblegarse, hacer reverencias, hacer una reverencia (hasta tocar el suelo con la frente), hacer una reverencia profunda, rendir pleitesía, someterse▒jucardus▒cwp1k593▓",
   "▓Citas y proverbios *▒Cásate con un arqueólogo. Cuanto más vieja te hagas, más encantadora te encontrará.▒Agatha Christie▒5mro58q9▓",
-  "▓Citas y proverbios *▒De todos los infortunios que afligen a la humanidad, el mรกs amargo es que hemos de tener conciencia de mucho y control de nada.▒Herodoto▒8yb4bp1y▓",
   "▓Citas y proverbios *▒Arrancad la esperanza del corazón del hombre y haréis de él un animal de presa.▒Marie Louise de la Ramée▒8kku93le▓",
   "▓Citas y proverbios *▒Ante las gentes de espíritu ligero nos avergonzamos de mostrar afectos profundos.▒Anne Louise Germaine de Stäel▒5ew4ctv8▓",
   "▓Citas y proverbios *▒Cada generación se sonríe de los padres, se ríe de los abuelos y admira a los bisabuelos.▒William Somerset Maugham▒ot0qylbo▓",
