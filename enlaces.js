f6dfa470333f3ecf0e486f44d434ab59c7ae94b5
Jucardus <197543233+jucardus@users.noreply.github.com>
2026-06-09 11:57:26 -0500

    Update enlaces.js from Google Apps Script

--- enlaces.js
+++ enlaces.js
@@ -1,4 +1,5 @@
 var enlaces = [
+  "▓Citas y proverbios *▒Es el cambio, no el amor, lo que hace avanzar al mundo. El amor sólo lo mantiene habitado.▒Anónimo▒w064z5h6▓",
   "▓Citas y proverbios *▒En un país bien gobernado, la pobreza es algo que avergüenza. En un país mal gobernado, la riqueza es algo que avergüenza.▒Confucio▒ozzmuub5▓",
   "▓Citas y proverbios *▒En todas épocas los pequeños han tenido que expiar por la tontería de los grandes.▒Jean de la Fontaine▒0ea7ka9t▓",
   "▓Citas y proverbios *▒De todos los infortunios que afligen a la humanidad, el más amargo es que hemos de tener conciencia de mucho y control de nada.▒Herodoto▒iut73o74▓",
